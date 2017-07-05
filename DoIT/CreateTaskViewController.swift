0d134c786aa688eed617816163698ddc3172f2a4
Vedi Chaudhri <vedichaudhri@gmail.com>
2017-07-05 11:10:27 -0700

    connected new buttons to code

---
+++ DoIT/CreateTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateTaskViewController.swift
+//  DoIT
+//
+//  Created by Vedi Chaudhri on 7/5/17.
+//  Copyright © 2017 Vedi Chaudhri. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameText: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+
+    
+
+}
