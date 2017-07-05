4c0836874084a801c842b81e04cc879dad78f00d
Vedi Chaudhri <vedichaudhri@gmail.com>
2017-07-05 10:18:35 -0700

    Initial Commit

---
+++ DoIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIT
+//
+//  Created by Vedi Chaudhri on 7/5/17.
+//  Copyright © 2017 Vedi Chaudhri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
